d5b06de12755aec0bd6bf43f5af09262b064a144
eddieedease <eddie@edease.nl>
2017-09-08 10:31:24 +0200

    first upload

---
+++ index.php
@@ -0,0 +1,29 @@
+<?php
+use \Psr\Http\Message\ServerRequestInterface as Request;
+use \Psr\Http\Message\ResponseInterface as Response;
+
+require 'vendor/autoload.php';
+
+
+$app = new \Slim\App;
+$app->get('/hello/{name}', function (Request $request, Response $response) {
+    $name = $request->getAttribute('name');
+    $response->getBody()->write("Hello, $name");
+    
+    return $response;
+});
+
+$app->get('/api/{name}', function (Request $request, Response $response) {
+    $name = $request->getAttribute('name');
+    $response->getBody()->write("EDease_ <b>Api</b> is running with argument:<br>  $name");
+   
+
+    //$data = array('name' => 'Bob', 'age' => 40);
+    //$newResponse = $oldResponse->withJson($data);
+
+    return $response;
+    //return $newResponse;
+});
+
+
+$app->run();
