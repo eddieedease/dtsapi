d1e3d8eae175290a1e56b25c2cd845dcd5fab473
eddieedease <eddie@edease.nl>
2017-09-10 19:41:42 +0200

    json response

--- index.php
+++ index.php
@@ -4,6 +4,9 @@ use \Psr\Http\Message\ResponseInterface as Response;
 
 require 'vendor/autoload.php';
 
+header("Content-Type: application/json");
+
+
 
 $app = new \Slim\App;
 $app->get('/hello/{name}', function (Request $request, Response $response) {
@@ -15,12 +18,12 @@ $app->get('/hello/{name}', function (Request $request, Response $response) {
 
 $app->get('/api/{name}', function (Request $request, Response $response) {
     $name = $request->getAttribute('name');
-    $response->getBody()->write("EDease_ <b>Api</b> is running with argument:<br>  $name");
+    $response->getBody()->write("EDease_ <b>Api</b> is running with argument:<br><br><br><br>$name");
    
 
-    //$data = array('name' => 'Bob', 'age' => 40);
+    $data = array('Jsonresponse' => 'item1', 'type' => '40X');
     //$newResponse = $oldResponse->withJson($data);
-
+    $response = json_encode($data);
     return $response;
     //return $newResponse;
 });
